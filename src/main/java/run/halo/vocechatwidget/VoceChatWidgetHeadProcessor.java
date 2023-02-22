dab4d70a9f16c1979249efcb6e65b8bd516a01f4
Colin Cai <colincai@outlook.com>
2023-02-22 06:02:35 +0000

    remove in-code line break

--- src/main/java/run/halo/vocechatwidget/VoceChatWidgetHeadProcessor.java
+++ src/main/java/run/halo/vocechatwidget/VoceChatWidgetHeadProcessor.java
@@ -40,18 +40,15 @@ public class VoceChatWidgetHeadProcessor implements TemplateHeadProcessor {
         String script = "";
         if(StringUtils.isNotBlank(config.getVocechat_url()) && StringUtils.isNotBlank(config.getVocechat_userid())){
             script = """
-                <script data-host-id="%s" 
-                src="%s/widget.js" 
-                data-close-width="48"
-                data-close-height="48"
+                <script async data-host-id="%s" src="%s/widget.js" data-close-width="48" data-close-height="48"
             """.formatted(config.getVocechat_userid(),config.getVocechat_url());
             if(StringUtils.isNotBlank(config.getVocechat_color()))
-                script = script + "data-theme-color=\"%s\"".formatted(config.getVocechat_color());
+                script = script + " data-theme-color=\"%s\"".formatted(config.getVocechat_color());
             if(StringUtils.isNotBlank(config.getVocechat_width()))
-                script = script + "data-open-width=\"%s\"".formatted(config.getVocechat_width());
+                script = script + " data-open-width=\"%s\"".formatted(config.getVocechat_width());
             if(StringUtils.isNotBlank(config.getVocechat_height()))
-                script = script + "data-open-height=\"%s\"".formatted(config.getVocechat_height());
-            script = script + " async /> ";
+                script = script + " data-open-height=\"%s\"".formatted(config.getVocechat_height());
+            script = script + "></script> ";
         }
         return script;
     }
